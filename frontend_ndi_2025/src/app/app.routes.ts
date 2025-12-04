8e60365945d32d521e9904594c2e64e66c079ab3
boudefoua <linaboudefoua12@gmail.com>
2025-12-05 00:53:47 +0100

    retro-visualizer

--- frontend_ndi_2025/src/app/app.routes.ts
+++ frontend_ndi_2025/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RetroVisualizerComponent } from './retro-visualizer/retro-visualizer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RetroVisualizerComponent },
+];
