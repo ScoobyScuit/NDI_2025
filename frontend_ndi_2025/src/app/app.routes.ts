e3585fc3af1391cd803f6db520fe31886ab2af14
choca64 <chocadu01@gmail.com>
2025-12-05 01:39:29 +0100

    Carte Talents tommy chouangmala

--- frontend_ndi_2025/src/app/app.routes.ts
+++ frontend_ndi_2025/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'landing-page',
+        pathMatch: 'full'
+    },
+    {
+        path: 'landing-page',
+        loadComponent: () => import('./defis/carte-talents/pages/landing-page/landing-page').then(m => m.LandingPage),
+        data: { animation: 'LandingPage' }
+    },
+    {
+        path: 'talents',
+        loadComponent: () => import('./defis/carte-talents/pages/talent-page/talent-page').then(m => m.TalentPage),
+        data: { animation: 'TalentsPage' }
+    },
+    {
+        path: 'talent-page',
+        redirectTo: 'talents',
+        pathMatch: 'full'
+    },
+    {
+        path: 'add-talent',
+        loadComponent: () => import('./defis/carte-talents/pages/add-talent-page/add-talent-page').then(m => m.AddTalentPage),
+        data: { animation: 'AddTalentPage' }
+    },
+    {
+        path: 'collaborations',
+        loadComponent: () => import('./defis/carte-talents/pages/collaborations-page/collaborations-page').then(m => m.CollaborationsPage),
+        data: { animation: 'CollaborationsPage' }
+    }
+];
