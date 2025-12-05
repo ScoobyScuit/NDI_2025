dc0c467338996acf737f4fed07bea4a3308d16a1
ScoobyScuit <bminet08@gmail.com>
2025-12-05 01:22:31 +0100

    Merge solved

--- frontend_ndi_2025/src/app/app.ts
+++ frontend_ndi_2025/src/app/app.ts
@@ -1,23 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatButtonComponent } from './defis/chat-bruti/component/chat-button/chat-button.component';
-import { ChatModalComponent } from './defis/chat-bruti/component/chat-modal/chat-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChatButtonComponent, ChatModalComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend_ndi_2025');
-  protected readonly isChatOpen = signal(false);
-
-  toggleChat() {
-    this.isChatOpen.update(value => !value);
-  }
-
-  closeChat() {
-    this.isChatOpen.set(false);
-  }
 }
